991385267af451fff15183a753df67bfb8d43a01
L0neGamer <benjaminbwm@gmail.com>
2019-12-03 18:30:34 +0000

    First star for day 3 done

--- day-03/main.hs
+++ day-03/main.hs
@@ -2,6 +2,7 @@ import System.IO
 import Control.Monad
 import Data.Map (Map)
 import qualified Data.Map as Map
+import qualified Data.Set as Set
 import Text.Read
 
 type Coord = (Int, Int)
@@ -20,7 +21,7 @@ main = do
 --            val' = execute 0 (Just (Map.insert 2 21 (Map.insert 1 76 testMap)))
 --            val = iter (map (\a -> (execute 0 (Just a))) (allPerms testMap)) 0 0
 --        print val
-        print "val"
+        print $ sol contents
 
 --iter :: [Maybe (Map Int Int)] -> Int -> Int -> Maybe (Int, Int)
 --iter [] noun verb = Nothing
@@ -65,43 +66,75 @@ maybeIfy [] = Just []
 maybeIfy (Nothing:xs) = Nothing
 maybeIfy ((Just a):xs) = maybeIfy xs >>= \x -> Just (a:x)
 
-toWire :: [Direction] -> Int -> Int -> Wire
-toWire [] _ _ = WireCons []
-toWire ((R, i):xs) x y = WireCons (x+i, y) (toWire xs (x+i) y)
+toWire :: [Direction] -> Coord -> Wire
+toWire dirs (x,y) = WireCons $ reverse $ foldl toCoord [(x, y)] dirs
 
---toWire dirs x y = WireCons $ foldl drawLine [(x, y)] dirs
+toCoord :: [Coord] -> Direction -> [Coord]
+toCoord coords dir = (toCoord' dir $ head coords) : coords
 
-toWire ((L, i):xs) x y = WireCons (x-i, y) (toWire xs (x-i) y)
-toWire ((U, i):xs) x y = WireCons (x, y+i) (toWire xs x (y+i))
-toWire ((D, i):xs) x y = WireCons (x, y-i) (toWire xs x (y-i))
-
---drawLine :: Direction -> [Coord] -> [Coord]
---drawLine dir coords = coords ++ (executeDir dir $ last coords)
---
---executeDir :: Direction -> Coord -> Coord
---executeDir (R, i) (x, y) = (x + i, y)
+toCoord' :: Direction -> Coord -> Coord
+toCoord' (R, i) (x, y) = (x + i, y)
+toCoord' (L, i) (x, y) = (x - i, y)
+toCoord' (U, i) (x, y) = (x, y + i)
+toCoord' (D, i) (x, y) = (x, y - i)
 
 fromInputToWires :: String -> [Maybe Wire]
 fromInputToWires str = wires
   where lines = splitStr str '\n'
         dirs = map (\a -> maybeIfy (fromStrLstToDirections (splitStr a ','))) lines
-        wires = map (\dir -> dir >>= \undir -> Just (toWire undir 0 0)) dirs
+        wires = map (\dir -> dir >>= \undir -> Just (toWire undir (0,0))) dirs
 
-findIntersection :: Coord -> Coord -> Coord -> Coord -> Maybe Coord
-findIntersection (x1,y1) (x2,y2) (x3,y3) (x4,y4)
-  | t >= 0 && t <= 1 = Just (round ((fromIntegral x1)+t*(fromIntegral (x2-x1))), round ((fromIntegral y1)+t*(fromIntegral (y2-y1))))
-  | otherwise = Nothing
-  where denom = (x1-x2)*(y3-y4) - (y1-y2)*(x3-x4)
-        t | denom == 0 = -1 :: Float
-          | otherwise = (fromIntegral ((x1-x3)*(y3-y4) - (y1-y3)*(x3-x4))) / (fromIntegral denom) :: Float
+sol inpStr = minimum $ map (manhattan (0,0)) inter
+  where wires = removeNothing $ fromInputToWires inpStr
+        inter = intersections (wires!!0) (wires!!1)
+
+manhattan (x1,y1) (x2,y2) = abs (x1-x2) + abs (y1-y2)
+
+--findIntersection :: Coord -> Coord -> Coord -> Coord -> Maybe Coord
+--findIntersection v1@(x1,y1) v2@(x2,y2) v3@(x3,y3) v4@(x4,y4)
+--  | x1 == x2 && y3 == y4 = findIntersection' (v1,v2) (v3,v4)
+--  | x3 == x4 && y1 == y2 = findIntersection' (v3,v4) (v1,v2)
+--  | otherwise = Nothing
+--
+--findIntersection' vert@((x1,y1),(x2,y2)) horiz@((x3,y3),(x4,y4))
+--  | vertCheck && horizCheck = Just (x1, y3)
+--  | otherwise = Nothing
+--  where vertCheck = (y1 >= y3 && y2 <= y3) || (y2 >= y3 && y1 <= y3)
+--        horizCheck = (x1 >= x3 && x2 <= x3) || (x2 >= x3 && x1 <= x3)
+--findIntersection (x1,y1) (x2,y2) (x3,y3) (x4,y4)
+--  | t >= 0 && t <= 1 = Just (round ((fromIntegral x1)+t*(fromIntegral (x2-x1))), round ((fromIntegral y1)+t*(fromIntegral (y2-y1))))
+--  | otherwise = Nothing
+--  where denom = (x1-x2)*(y3-y4) - (y1-y2)*(x3-x4)
+--        t | denom == 0 = -1 :: Float
+--          | otherwise = (fromIntegral ((x1-x3)*(y3-y4) - (y1-y3)*(x3-x4))) / (fromIntegral denom) :: Float
 --  where t = div ((x1-x2)(y2-1) - (y1-y2)(x2-1)) ((x1-1)(y2-1) - (y1-1)(x2-1))
 
-intersections :: Wire -> Wire -> [Maybe Coord]
-intersections (WireEnd) _ = []
-intersections _ (WireEnd) = []
-intersections (WireCons _ WireEnd) _ = []
-intersections _ (WireCons _ WireEnd) = []
-intersections (WireCons coord1 (WireCons coord1' wire1)) (WireCons coord2 (WireCons coord2' wire2)) = findIntersection coord1 coord1' coord2 coord2' : intersections (WireCons coord1' wire1) (WireCons coord2' wire2) ++ intersections (WireCons coord1 (WireCons coord1' wire1)) (WireCons coord2' wire2) ++ intersections (WireCons coord1' wire1) (WireCons coord2 (WireCons coord2' wire2))
+genSet :: Coord -> Coord -> Set.Set Coord
+genSet a@(ax,ay) b@(bx,by) = Set.fromList [(x,y) | x <- [xbot..xtop], y <- [ybot..ytop]]
+  where xbot = min ax bx
+        xtop = max ax bx
+        ybot = min ay by
+        ytop = max ay by
+
+genSetCoords :: [Coord] -> Set.Set Coord
+genSetCoords coords@(a:b:xs) = Set.union (genSet a b) (genSetCoords (b:xs))
+genSetCoords _ = Set.empty
+
+extract (WireCons xs) = xs
+
+genSetWire :: Wire -> Set.Set Coord
+genSetWire (WireCons (a:b:xs)) = Set.union (genSet a b) (genSetWire $ WireCons (b:xs))
+genSetWire (WireCons _) =  Set.empty
+
+intersections :: Wire -> Wire -> [Coord]
+intersections (WireCons w1) (WireCons w2) = Set.toList $ Set.intersection (genSetCoords $ tail w1) (genSetCoords $ tail w2)
+--intersections (WireCons (x1:x2:xs)) (WireCons (y1:y2:ys)) = findIntersection x1 x2 y1 y2 : intersections (WireCons (x2:xs)) (WireCons (y2:ys)) ++ intersections (WireCons (x1:x2:xs)) (WireCons (y2:ys)) ++ intersections (WireCons (x2:xs)) (WireCons (y1:y2:ys))
+--intersections _ _ = []
+--intersections (WireEnd) _ = []
+--intersections _ (WireEnd) = []
+--intersections (WireCons _ WireEnd) _ = []
+--intersections _ (WireCons _ WireEnd) = []
+--intersections (WireCons coord1 (WireCons coord1' wire1)) (WireCons coord2 (WireCons coord2' wire2)) = findIntersection coord1 coord1' coord2 coord2' : intersections (WireCons coord1' wire1) (WireCons coord2' wire2) ++ intersections (WireCons coord1 (WireCons coord1' wire1)) (WireCons coord2' wire2) ++ intersections (WireCons coord1' wire1) (WireCons coord2 (WireCons coord2' wire2))
 
 removeNothing :: [Maybe a] -> [a]
 removeNothing [] = []
@@ -118,7 +151,7 @@ removeDupes' (a:as) xs
 
 findAllIntersections :: [Wire] -> [Coord]
 findAllIntersections [] = []
-findAllIntersections xs = removeDupes $ removeNothing $ intersections (xs!!0) (xs!!1) --concat $ map (\(w1, w2) -> intersections w1 w2) [(w1, w2)| w1 <- xs, w2 <- xs, not $w1 == w2]
+findAllIntersections xs = removeDupes $ intersections (xs!!0) (xs!!1) --concat $ map (\(w1, w2) -> intersections w1 w2) [(w1, w2)| w1 <- xs, w2 <- xs, not $w1 == w2]
 
 --toWire :: [Direction] -> Wire
 --
