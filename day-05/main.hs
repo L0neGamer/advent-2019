1da7064c177ed89c9e1f00fbc30a3b351414da11
L0neGamer <benjaminbwm@gmail.com>
2019-12-05 16:40:17 +0000

    Realised current approach is not useful for day 5, starting over

--- day-05/main.hs
+++ day-05/main.hs
@@ -12,28 +12,65 @@ wordsWhen p s =  case dropWhile p s of
 
 --data ParamRel = Rel Int
 --data ParamAbs = Abs Int
-data Param = Rel Int | Abs Int
-data Op = Input Int -- writes to this Int
-        | Output Param
-        | Mul Param Param Int
-        | Add Param Param Int
+data Param = Rel | Abs deriving Show
+data Op = Input -- Int -- writes to this Int
+        | Output Param -- Param
+        | Mul Param Param -- Param Param Int
+        | Add Param Param -- Param Param Int
+        | Halt deriving Show
+data Register = RegO Op
+              | RegI Int deriving Show
 
 main = do
         contents <- readFile "input.txt"
         print "arg"
-        let testMap = fromStr contents
+--        let testMap = fromStr contents
 --            val' = execute 0 (Just (Map.insert 2 21 (Map.insert 1 76 testMap)))
 --            val = iter (map (\a -> (execute 0 (Just a))) (allPerms testMap)) 0 0
 --        print val
 --        print val'
 
+fromInput :: String -> Map Int Register
+fromInput str = parseList 0 (fromStr str)
+
 fromStr :: String -> [Int]
 fromStr str = map (read) stringLst
   where stringLst = wordsWhen (==',') str
 
---parseList :: [Int] -> Map Int Op
---parseList (x:xs)
---  |
+enumerate :: [a] -> [(Int, a)]
+enumerate xs = enumerate' xs 0
+
+enumerate' :: [a] -> Int -> [(Int, a)]
+enumerate' [] _ = []
+enumerate' (x:xs) i = (i, x):enumerate' xs (i+1)
+
+parseList :: Int -> [Int] -> Map Int Register
+parseList i [] = Map.fromList []
+parseList i (x:xs) = parsedList
+  where parsedOp = parseOp (show x) xs
+        processedOps = (1 + length xs) - (length $ fst parsedOp)
+
+        parsedList = Map.union (parseList (i + processedOps) (snd parsedOp)) (Map.fromList (enumerate' (fst parsedOp) i))
+
+getParamOp :: Char -> Param
+getParamOp '0' = Rel
+getParamOp '1' = Abs
+
+parseOp :: String -> [Int] -> ([Register], [Int])
+parseOp [x] ops = parseOp ['0',x] ops
+parseOp [y,x] ops = constructOp (read [y,x]) Rel Rel ops
+parseOp (x':y:[x]) ops = constructOp (read [y,x]) (getParamOp x') Rel ops
+parseOp (x'':x':y:[x]) ops = constructOp (read [y,x]) (getParamOp x') (getParamOp x'') ops
+parseOp xs ops = ([RegI (read xs)], ops)
+
+constructOp :: Int -> Param -> Param -> [Int] -> ([Register], [Int])
+constructOp 1 p1 p2 (x:x':x'':xs) = (RegO (Add p1 p2):RegI x:RegI x':RegI x'':[], xs)
+constructOp 2 p1 p2 (x:x':x'':xs) = (RegO (Mul p1 p2):RegI x:RegI x':RegI x'':[], xs)
+constructOp 3 _ _ (x:xs) = (RegO Input:RegI x:[], xs)
+constructOp 4 p1 _ (x:xs) = (RegO (Output p1):RegI x:[], xs)
+constructOp 99 _ _ xs = ([RegO Halt], xs)
+constructOp x _ _ xs = ([RegI x], xs) --error $ "error on " ++ show x ++ ", lst:" ++ show xs
+
 
 --iter :: [Maybe (Map Int Int)] -> Int -> Int -> Maybe (Int, Int)
 --iter [] noun verb = Nothing
