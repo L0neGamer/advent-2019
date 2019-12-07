1b04f5a7f63b166af635975a0822e5e9718f01e1
Benjamin <benjaminbwm@gmail.com>
2019-12-07 16:56:53 +0000

    Further tidying and moving around of day 7

--- day-07/main.hs
+++ day-07/main.hs
@@ -5,6 +5,7 @@ import qualified Data.Map as Map
 import Debug.Trace
 import Data.List
 import Data.Maybe
+import System.CPUTime
 
 ---- thanks to https://stackoverflow.com/questions/4978578/how-to-split-a-string-in-haskell
 wordsWhen     :: (Char -> Bool) -> String -> [String]
@@ -12,6 +13,12 @@ wordsWhen p s =  case dropWhile p s of
                       "" -> []
                       s' -> w : wordsWhen p s''
                             where (w, s'') = break p s'
+
+timeDif :: Integer -> IO Integer
+timeDif x = do
+             y <- getCPUTime
+             print $ (fromIntegral (y-x))/(10^9)
+             return y
 
 data Param = Rel | Abs deriving Show
 data Op = Input -- Int
@@ -23,7 +30,7 @@ data Op = Input -- Int
 type Mem = Map Int Int
 type ProgramCounter = Int
 type InputVals = [Int]
-type OutputVals = [String]
+type OutputVals = [Int]
 data EndState = Halted | AwaitInput | Running deriving (Show, Eq)
 data ProgramState = ProgStat
   { mem            :: Mem
@@ -44,20 +51,25 @@ main = do
         let four = [0..4]
             combos = permutations four
             combos' = permutations [5..9]
---            res = sort $ map (\inp -> (runForAmplifiersPt1 inp 0 contents, inp)) combos
+            res = sort $ map (\inp -> (runForAmplifiersPt1 inp 0 contents, inp)) combos
 --            res = sort $ map (\inp -> (runForAmplifiers inp 0 contentsT1, inp)) combos
-            res = sort $ map (\inp -> (runForAmplifiersPt2 inp [Nothing | _ <- four] 0 0 contents)) combos'
+            res2 = sort $ map (\inp -> (runForAmplifiersPt2 inp [Nothing | _ <- four] 0 0 contents, inp)) combos'
 --            res' = runStr contents [4,3,2,1,0]
 --        print combos
 --        print res'
-        print $ runForAmplifiersPt1 [4,3,2,1,0] 0 contentsT1
-        print $ runForAmplifiersPt1 [0,1,2,3,4] 0 contentsT2
-        print $ runForAmplifiersPt1 [1,0,4,3,2] 0 contentsT3
-        print $ runForAmplifiersPt2 [9,8,7,6,5] [Nothing | _ <- four] 0 0 contentsT4
-        print res
+        start <- getCPUTime
+--        print $ runForAmplifiersPt1 [4,3,2,1,0] 0 contentsT1
+--        print $ runForAmplifiersPt1 [0,1,2,3,4] 0 contentsT2
+--        print $ runForAmplifiersPt1 [1,0,4,3,2] 0 contentsT3
+--        print $ runForAmplifiersPt2 [9,8,7,6,5] [Nothing | _ <- four] 0 0 contentsT4
+        print $ last res
+        middle <- timeDif start
+        print $ last res2
+        end <- timeDif middle
+        print $ ""
 
 getOutput :: ProgramState -> Int
-getOutput ps = read $ head $ outputVals ps
+getOutput ps = head $ outputVals ps
 
 runForAmplifiersPt1 :: [Int] -> Int -> String -> Int
 runForAmplifiersPt1 [] prev _ = prev
@@ -72,28 +84,24 @@ checkProgress (Just (ProgStat _ _ _ _ Halted):xs) = checkProgress xs
 checkProgress [] = True
 checkProgress xs = False
 
-changeInput (ProgStat mem pc inp out stop) xs = ProgStat mem pc xs out Running
-
 runForAmplifiersPt2 :: [Int] -> [Maybe ProgramState] -> Int -> Int -> String -> Int
-runForAmplifiersPt2 phases pss phaseIndex prev str = x
+runForAmplifiersPt2 phases pss phaseIndex prev str
+  | checkProgress newPss = outVal
+  | otherwise = runForAmplifiersPt2 phases newPss nextIndex outVal str
   where maybePs = pss!!phaseIndex
         ps | isNothing maybePs = runStr str ((phases!!phaseIndex):[prev])
-           | otherwise = runProg (changeInput (fromJust maybePs) [prev])
+           | otherwise = runProg (setEndState (setInput (fromJust maybePs) [prev]) Running)
         outVal = getOutput ps
         newPss = replaceAt phaseIndex pss (Just ps)
         nextIndex = mod (phaseIndex + 1) (length phases)
-        x | checkProgress newPss = outVal
-          | otherwise = runForAmplifiersPt2 phases newPss nextIndex outVal str
 
 runStr :: String -> [Int] -> ProgramState
 runStr str inputs = runProg $ ProgStat (fromInput str) 0 inputs [] Running
 
 runProg :: ProgramState -> ProgramState
-runProg ps@(ProgStat mem pc _ _ _) = next
+runProg ps@(ProgStat mem pc _ _ Running) = runProg (runOp op ps)
   where op = parseOp $ (show $ mem Map.! pc)
-        ps'@(ProgStat mem' pc' _ _ state) = runOp op ps
-        next | Running == state = runProg ps'
-             | otherwise = ps'
+runProg ps = ps
 
 runBinOp :: (Int -> Int -> Int) -> Param -> Param -> ProgramState -> ProgramState
 runBinOp f p1 p2 (ProgStat mem pc inp out _) = ProgStat mem' (pc + 4) inp out Running
@@ -102,20 +110,24 @@ runBinOp f p1 p2 (ProgStat mem pc inp out _) = ProgStat mem' (pc + 4) inp out Ru
         ins = getItem Abs (pc + 3) mem
         mem' = Map.insert ins (f x y) mem
 
-runJmp f p1 p2 (ProgStat mem pc inp out _)
-  | f x = ProgStat mem (getItem p2 (pc + 2) mem) inp out Running
-  | otherwise = ProgStat mem (pc + 3) inp out Running
-  where x = getItem p1 (pc + 1) mem
+runJmp :: (Int -> Bool) -> Param -> Param -> ProgramState -> ProgramState
+runJmp f p1 p2 ps@(ProgStat mem pc inp out _) = setProgramCounter ps pc'
+  where pc' | f (getItem p1 (pc + 1) mem) = getItem p2 (pc + 2) mem
+            | otherwise = pc + 3
 
 boolFToInt f a b = fromEnum (f a b)
+
+setInput (ProgStat mem pc _ out es) inp = ProgStat mem pc inp out es
+setEndState (ProgStat mem pc inp out _) es = ProgStat mem pc inp out es
+setProgramCounter (ProgStat mem _ inp out es) pc = ProgStat mem pc inp out es
 
 runOp :: Op -> ProgramState -> ProgramState
 runOp (BinOp f p1 p2) ps = runBinOp f p1 p2 ps
 runOp (JmpNZ p1 p2)   ps = runJmp (/= 0) p1 p2 ps
 runOp (JmpOZ p1 p2)   ps = runJmp (== 0) p1 p2 ps
-runOp (Halt)          (ProgStat mem pc inp out _) = ProgStat mem pc inp out Halted
-runOp (Output p1)     (ProgStat mem pc inp out _) = ProgStat mem (pc + 2) inp ((show (getItem p1 (pc + 1) mem)):out) Running
-runOp (Input)         ps@(ProgStat mem pc [] out _) = ProgStat mem pc [] out AwaitInput
+runOp (Halt)          ps = setEndState ps Halted
+runOp (Output p1)     (ProgStat mem pc inp out _) = ProgStat mem (pc + 2) inp ((getItem p1 (pc + 1) mem):out) Running
+runOp (Input)         ps@(ProgStat _ _ [] _ _) = setEndState ps AwaitInput
 runOp (Input)         (ProgStat mem pc (i:inp) out _) = ProgStat mem' (pc + 2) inp out Running
   where mem' = Map.insert (getItem Abs (pc + 1) mem) i mem
 
