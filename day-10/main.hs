29f38b095b9cb27d1218ed6d48185ae8945e5b72
Benjamin <benjaminbwm@gmail.com>
2019-12-10 13:50:48 +0000

    Made some changes towards ratios for day 10

--- day-10/main.hs
+++ day-10/main.hs
@@ -6,6 +6,7 @@ import Data.List
 import Data.Maybe
 import System.CPUTime
 import Data.Fixed (mod')
+import Data.Ratio
 
 ---- thanks to https://stackoverflow.com/questions/4978578/how-to-split-a-string-in-haskell
 wordsWhen     :: (Char -> Bool) -> String -> [String]
@@ -34,30 +35,25 @@ xor True False = True
 xor False True = True
 xor _ _ = False
 
-isNeg :: (Num a, Ord a) => a -> Bool
-isNeg x = x < 0
+getSign :: (Num a, Ord a, Integral a) => a -> a
+getSign a = floor (a' / (abs a'))
+  where a' = fromIntegral a
 
 type Coord = (Int, Int)
-data MyFrac = MFrac Int Int deriving (Show)
-
-instance Eq MyFrac where
-  (MFrac x y) == (MFrac x' y') = x==x' && y==y'
-
-instance Ord MyFrac where
-  m@(MFrac x y) `compare` m'@(MFrac x' y')
-   | m == m' = EQ
-   | y == y' = x `compare` x'
-   | otherwise = y `compare` y'
+type MyFrac = (Ratio Int, Ordering)
 
 main = do
         contents <- readFile "input.txt"
+--        contents <- readFile "inputtest3.txt"
         let asteroids = getAsteroids contents
             anglesFromStart = map (angleFrom (head asteroids)) (tail asteroids)
             visibleFrom = map (\a -> (length (getVisible a asteroids), getVisible a asteroids, a)) asteroids
             center@(t,as',c) = last $ sort visibleFrom
+            picked = cherryPick $ findVaporised c (closestTo c asteroids)
         start <- getCPUTime
         print center
-        print $ (cherryPick $ findVaporised c (closestTo c asteroids))!!199
+        print $ picked
+        print $ picked!!199
         middle <- timeDif start
         end <- timeDif middle
         putStr ""
@@ -66,10 +62,9 @@ replaceAt n xs x = fst splitLst ++ [x] ++ (tail $ snd splitLst)
   where splitLst = splitAt (fromInteger n) xs
 
 consMyFrac :: Int -> Int -> MyFrac
-consMyFrac x y = MFrac (div x gcd'') (div y gcd'')
-  where gcd' = gcd x y
-        gcd'' | gcd' == 0 = 1
-              | otherwise = gcd'
+consMyFrac x y
+  | y == 0 = (getSign x % 1, EQ)
+  | otherwise = (x % y, y `compare` 0)
 
 angleFrom :: Coord -> Coord -> MyFrac
 angleFrom (x, y) (x', y') = consMyFrac x'' y''
@@ -111,21 +106,6 @@ findVaporised :: Coord -> [Coord] -> [[Coord]]
 findVaporised o as = map snd $ findVaporised'' $ M.toList $ findVaporised' o (closestTo o as')
   where as' = delete o as
 
-findVaporised'' :: [(MyFrac, [Coord])] -> [(MyFrac, [Coord])]
-findVaporised'' as = sortBy (\(m,_) (m',_) -> clockWiseOrder m m') as
-
-cherryPick :: [[a]] -> [a]
-cherryPick as = lst ++ (cherryPick'' as')
-  where (lst, as') = cherryPick' as
-        cherryPick'' [] = []
-        cherryPick'' as'' = cherryPick as'
-
-cherryPick' :: [[a]] -> ([a], [[a]])
-cherryPick' [] = ([], [])
-cherryPick' ([]:as) = cherryPick' as
-cherryPick' ((x:xs):as) = (x:lhs, xs:rhs)
-  where (lhs, rhs) = cherryPick' as
-
 findVaporised' :: Coord -> [Coord] -> M.Map MyFrac [Coord]
 findVaporised' o [] = M.empty
 findVaporised' o (a:as) = M.insert myFrac (a:prev) next
@@ -133,11 +113,29 @@ findVaporised' o (a:as) = M.insert myFrac (a:prev) next
         myFrac = angleFrom o a
         prev = M.findWithDefault [] myFrac next
 
+findVaporised'' :: [(MyFrac, [Coord])] -> [(MyFrac, [Coord])]
+findVaporised'' as = sortBy (\(m,_) (m',_) -> clockWiseOrder m m') as
+
+cherryPick :: [[a]] -> [a]
+cherryPick [] = []
+cherryPick as = lst ++ (cherryPick as')
+  where (lst, as') = cherryPick' as
+
+cherryPick' :: [[a]] -> ([a], [[a]])
+cherryPick' [] = ([], [])
+cherryPick' ([]:as) = cherryPick' as
+cherryPick' ((x:xs):as) = (x:lhs, xs:rhs)
+  where (lhs, rhs) = cherryPick' as
+
 toAngle :: RealFloat a => MyFrac -> a
-toAngle m@(MFrac x y) = mod' ((atan2 (y') x' + (pi/2)) + (2*pi)) (2 * pi)
-  where x' = fromIntegral x
-        y' = fromIntegral y
+toAngle (m, o) = mod' ((atan2 y' x' + (pi/2)) + (2*pi)) (2 * pi)
+  where x'' = fromIntegral $ numerator m
+        y'' = fromIntegral $ denominator m
+        x' | o == LT && y'' > 0 = -x''
+           | otherwise = x''
+        y' | o == LT && y'' > 0 = -y''
+           | o == EQ = 0
+           | otherwise = y''
 
 clockWiseOrder :: MyFrac -> MyFrac -> Ordering
 clockWiseOrder m m' = toAngle m `compare` toAngle m'
-
