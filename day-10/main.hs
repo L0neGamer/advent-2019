44bb36bd0a9c569f6f0012417245a0d4d3d2e20b
Benjamin <benjaminbwm@gmail.com>
2019-12-10 12:29:14 +0000

    day 10 complete

---
+++ day-10/main.hs
@@ -0,0 +1,171 @@
+import System.IO
+import Control.Monad
+import qualified Data.Map as M
+import qualified Data.Set as S
+import Data.List
+import Data.Maybe
+import System.CPUTime
+import Data.Fixed (mod')
+
+---- thanks to https://stackoverflow.com/questions/4978578/how-to-split-a-string-in-haskell
+wordsWhen     :: (Char -> Bool) -> String -> [String]
+wordsWhen p s =  case dropWhile p s of
+                      "" -> []
+                      s' -> w : wordsWhen p s''
+                            where (w, s'') = break p s'
+
+timeDif :: Integer -> IO Integer
+timeDif x = do
+             y <- getCPUTime
+             print $ (fromIntegral (y-x))/(10^9)
+             return y
+
+fromStr :: String -> Char -> [String]
+fromStr str c = wordsWhen (==c) str
+
+enumerate :: [a] -> [(Integer, a)]
+enumerate xs = enumerate' xs 0
+
+enumerate' :: [a] -> Integer -> [(Integer, a)]
+enumerate' [] _ = []
+enumerate' (x:xs) i = (i, x):enumerate' xs (i+1)
+
+xor True False = True
+xor False True = True
+xor _ _ = False
+
+isNeg :: (Num a, Ord a) => a -> Bool
+isNeg x = x < 0
+
+type Coord = (Int, Int)
+data MyFrac = MFrac Int Int deriving (Show)
+
+instance Eq MyFrac where
+  (MFrac x y) == (MFrac x' y') = x==x' && y==y'
+--    where isNegTrue = (not (isNeg x `xor` isNeg x')) && (not (isNeg y `xor` isNeg y'))
+--  (MFrac x y) == (MFrac x' y') = isNegTrue && (((fromIntegral x)/(fromIntegral y)) == ((fromIntegral x')/(fromIntegral y')))
+--    where isNegTrue = (not (isNeg x `xor` isNeg x')) && (not (isNeg y `xor` isNeg y'))
+
+instance Ord MyFrac where
+  m@(MFrac x y) `compare` m'@(MFrac x' y')
+   | m == m' = EQ
+   | y == y' = x `compare` x'
+   | otherwise = y `compare` y'
+--   | otherwise = ((fromIntegral x)/(fromIntegral y)) `compare` ((fromIntegral x')/(fromIntegral y'))
+
+main = do
+        contents <- readFile "input.txt"
+--        contents <- readFile "inputtest1.txt"
+--        contents <- readFile "inputtest2.txt"
+--        contents <- readFile "inputtest3.txt"
+        let asteroids = getAsteroids contents
+            anglesFromStart = map (angleFrom (head asteroids)) (tail asteroids)
+            visibleFrom = map (\a -> (length (getVisible a asteroids), getVisible a asteroids, a)) asteroids
+            mem = (getVisible' (1,2) (closestTo (1,2) asteroids))
+            center@(t,as',c) = last $ sort visibleFrom
+        start <- getCPUTime
+--        print $ closestTo (head asteroids) (tail asteroids)
+--        print $ getVisible (head asteroids) (tail asteroids)
+--        print $ last $ sort visibleFrom
+--        print $ (cherryPick $ findVaporised (11,13) asteroids)
+        print center
+        print $ (cherryPick $ findVaporised c (closestTo c asteroids))!!199
+--        print $ findVaporised'' $ M.toList $ findVaporised' (11,13) (closestTo (11,13) asteroids)
+--        print mem
+--        print $mem M.! (MFrac 0 1)
+        middle <- timeDif start
+        end <- timeDif middle
+        putStr ""
+
+replaceAt n xs x = fst splitLst ++ [x] ++ (tail $ snd splitLst)
+  where splitLst = splitAt (fromInteger n) xs
+
+consMyFrac :: Int -> Int -> MyFrac
+consMyFrac x y = MFrac (div x gcd'') (div y gcd'')
+  where gcd' = gcd x y
+        gcd'' | gcd' == 0 = 1
+              | otherwise = gcd'
+
+angleFrom :: Coord -> Coord -> MyFrac
+angleFrom (x, y) (x', y') = consMyFrac x'' y''
+  where x'' = x' - x
+        y'' = y' - y
+
+manhattan :: Coord -> Coord -> Int
+manhattan (x,y) (x',y') = (abs (x - x')) + (abs (y - y'))
+
+findAsteroids :: String -> Int -> Int -> [Coord]
+findAsteroids [] x y = []
+findAsteroids (s:ss) x y
+  | s == '#' = (x,y) : findAsteroids ss (x+1) y
+  | otherwise = findAsteroids ss (x+1) y
+
+getAsteroids :: String -> [Coord]
+getAsteroids str = asteroids
+  where rows = fromStr str '\n'
+        asteroids = concat [findAsteroids (rows!!y) 0 y | y <- [0..((length rows) - 1)]]
+
+getVisible :: Coord -> [Coord] -> S.Set Coord
+getVisible o as = fromMapToSet $ getVisible' o (closestTo o as')
+  where as' = delete o as
+
+getVisible' :: Coord -> [Coord] -> M.Map MyFrac Coord
+getVisible' o [] = M.empty
+getVisible' o (a:as) = space
+  where rest = getVisible' o as
+        space = M.insert (angleFrom o a) a rest
+
+closestTo :: Coord -> [Coord] -> [Coord]
+closestTo o = sortBy (\a b -> (closestTo' a) `compare` (closestTo' b))
+  where closestTo' = manhattan o
+
+fromMapToSet :: Ord b => M.Map a b -> S.Set b
+fromMapToSet m = S.fromList $ M.elems m
+
+findVaporised :: Coord -> [Coord] -> [[Coord]]
+findVaporised o as = map snd $ findVaporised'' $ M.toList $ findVaporised' o (closestTo o as')
+  where as' = delete o as
+
+findVaporised'' :: [(MyFrac, [Coord])] -> [(MyFrac, [Coord])]
+findVaporised'' as = sortBy (\(m,_) (m',_) -> clockWiseOrder m m') as
+
+cherryPick :: [[a]] -> [a]
+cherryPick as = lst ++ (cherryPick'' as')
+  where (lst, as') = cherryPick' as
+        cherryPick'' [] = []
+        cherryPick'' as'' = cherryPick as'
+
+cherryPick' :: [[a]] -> ([a], [[a]])
+cherryPick' [] = ([], [])
+cherryPick' ([]:as) = cherryPick' as
+cherryPick' ((x:xs):as) = (x:lhs, xs:rhs)
+  where (lhs, rhs) = cherryPick' as
+
+findVaporised' :: Coord -> [Coord] -> M.Map MyFrac [Coord]
+findVaporised' o [] = M.empty
+findVaporised' o (a:as) = M.insert myFrac (a:prev) next
+  where next = findVaporised' o as
+        myFrac = angleFrom o a
+        prev = M.findWithDefault [] myFrac next
+
+toAngle :: RealFloat a => MyFrac -> a
+toAngle m@(MFrac x y) = mod' (((atan2 (y') x' + (pi/2))) + (20*pi)) (2 * pi)
+--  | not (isNeg x) && not (isNeg y) = atan (x'/y')
+--  | isNeg x && isNeg y = pi + atan (x'/y')
+--  | otherwise = atan
+  where x' = fromIntegral x
+        y' = fromIntegral y
+
+clockWiseOrder :: MyFrac -> MyFrac -> Ordering
+clockWiseOrder m@(MFrac x y) m'@(MFrac x' y') = toAngle m `compare` toAngle m'
+--  | m == m' = EQ
+--  | x == x' = specialCompare y y'
+--  | otherwise = specialCompare x x'
+
+--specialCompare :: (Num a, Ord a) => a -> a -> Ordering
+--specialCompare a b
+--  | isNeg a && isNeg b = (abs a) `compare` (abs b)
+--  | isNeg a && not (isNeg b) = GT
+--  | not (isNeg a) && isNeg b = LT
+--  | otherwise = a `compare` b
+
